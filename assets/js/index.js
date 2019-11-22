9cd49ea24260ccb4c3b42489d3bf2bf488953a56
kshep425 <9432996+kshep425@users.noreply.github.com>
2019-11-21 21:30:02 -0500

    Added query and post methods

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,48 @@
 const wger_api = {
     key: "25d6f241707205c0e6e01e38c8a191d27f8b1b8d",
-    uri: "https://wger.de/api/v2/workout/"
+    uri: "https://wger.de/api/v2/"
 }
 
 const query_data = {
-
+    format: "json",
+    status: "2",
+    language: "2"
 }
 
-const wgr_query = {
+const wger_query = function(endpoint){
+
     $.ajax({
-        url: wgr_api,
-       // data: query_data,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader ("Authorization", "Token " + wger_api.key);
+        },
+        url: wger_api.uri + endpoint,
+        //data: query_data,
         method: "GET"
     }).then(function(response){
         console.log(response);
+        return response
+    })
+}
+
+const post_data = {
+    "license_author": "Survival of the Fittest",
+    "description": "Basic crunch: To do a crunch, start by lying on the floor with your knees bent and your feet flat on the ground. Then, cross your arms over your chest. When you're ready, lift your shoulders off the mat while contracting your abs and exhaling. Hold this pose for 1-2 seconds, then inhale and slowly lower back down.",
+    "name": "Basic Crunch",
+    "name_original": "Basic Crunch",
+    "license": 2,
+    "category": 10,
+    "language": 2,
+    "muscles": [14],
+    "muscles_secondary": [],
+    "equipment": []
+}
+const wger_post = function(endpoint){
+    $.ajax({
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader ("Authorization", "Token " + wger_api.key);
+        },
+        url: wger_api.uri + endpoint + "/",
+        data: post_data,
+        method: "POST"
     })
 }
