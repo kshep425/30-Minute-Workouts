453621dcb86cd1475ddf3e2866c390d81df89194
kshep425 <9432996+kshep425@users.noreply.github.com>
2019-11-23 13:00:32 -0500

    When user clicks start, workout starts

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,24 @@ function excercise() {
 }
 
 //wger_query("exercise");
-wger_query("exercise/345");
+$("#start").click(function start_exercise(){
+    $("#work_out_page").css({"display":"block"})
+    $("#profile").css({"display":"none"})
+    excercise_ids = [91];
+    excercise_ids.forEach(id => {
+        wger_query("exercise/91");
+        //let  = 45;
+        display_time(45)
+    });
+
+})
+
+function display_time(time_left){
+    setInterval(function(){
+
+    $("#timer_section").text(time_left)
+    if (time_left <= 0 ){
+        clearInterval()
+    }
+    time_left = time_left-1
+},1000)}
