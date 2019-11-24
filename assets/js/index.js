a8ab1acc873fc93a90ea021645ddfea9777542ea
kshep425 <9432996+kshep425@users.noreply.github.com>
2019-11-24 11:13:35 -0500

    Added comments and documentation comments, moved non-working post to a different file, added timer functions for display, and break

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,45 @@
+/** The query url and api are preset to be called. */
 const wger_api = {
     key: "25d6f241707205c0e6e01e38c8a191d27f8b1b8d",
     uri: "https://wger.de/api/v2/"
 }
 
+/** *The query data can be updated as needed.
+*  For endpoint 'exercise/id': format and status;
+*/
 const query_data = {
     format: "json",
     status: "2",
 }
 
+/**
+ * Get exercises using wger_query
+ * @param {string} endpoint
+ */
 const wger_query = function(endpoint){
 
     $.ajax({
         beforeSend: function (xhr) {
             xhr.setRequestHeader ("Authorization", "Token " + wger_api.key);
+            xhr.setRequestHeader ("Accept", "application/json;indent=4")
         },
         url: wger_api.uri + endpoint,
         data: query_data,
         method: "GET"
     }).then(function(response){
         console.log(response);
-        set_exercises(response)
-        return response
+        set_exercises(response);
+    }).fail(function(err){
+        console.log("Query Failed!")
+        console.log(err)
+        alert("Query Failed!")
     })
 }
 
-
+/**
+ * Set Exercise info to display during time interval
+ * @param {object} exercise This is the response from the exercise/id endpoint
+ */
 function set_exercises(exercise){
     ex = {
         name: exercise.name,
@@ -36,26 +51,56 @@ function set_exercises(exercise){
     $("#exercise_description").text(ex.description)
 }
 
-
-const post_data = {
-    "license_author": "Survival of the Fittest",
-    "description": "Basic crunch: To do a crunch, start by lying on the floor with your knees bent and your feet flat on the ground. Then, cross your arms over your chest. When you're ready, lift your shoulders off the mat while contracting your abs and exhaling. Hold this pose for 1-2 seconds, then inhale and slowly lower back down.",
-    "name": "Basic Crunch",
-    "name_original": "Basic Crunch",
-    "license": 2,
-    "category": 10,
-    "language": 2,
-    "muscles": [14],
-    "muscles_secondary": [],
-    "equipment": []
+/**
+ * It's Break Time!
+ * This will:
+ * - Display the time left for the break
+ * - change the exercise name to break;
+ *
+ * TODO: add next exercise name.
+ * TODO: Get break time from profile settings.
+ *
+ */
+function its_break_time(){
+    let break_time = 15;
+    $("#exercise_name").text("BREAK TIME!")
+    $("#exercise_description").text("Next exercise is: ")
+    display_time(break_time, "#exercise_break_section")
 }
-const wger_post = function(endpoint){
-    $.ajax({
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader ("Authorization", "Token " + wger_api.key);
-        },
-        url: wger_api.uri + endpoint + "/",
-        data: post_data,
-        method: "POST"
-    })
+/**
+ * Display Time
+ * @param {Number} time_left must be in seconds
+ * @param {string} section section needs to include # or . example: ('#exercise_timer_section')
+ *
+ * TODO: Add 3,2,1 beeps with big number flashes when timer is ending
+ *
+ */
+function display_time(time_left, section){
+    console.log("Display " + time_left + " on " + section)
+    let exercise_interval = setInterval(() => {
+        $(section).text(convert_seconds_to_time(time_left))
+        if (time_left <= 0 ){
+            clearInterval(exercise_interval)
+        }
+        time_left = time_left-1
+    }, 1000)
+}
+
+/**  Convert seconds to Time
+* @param {number} given_seconds
+* @returns {string} Time String formatted hh:mm:ss
+* referenced: https://www.geeksforgeeks.org/how-to-convert-seconds-to-time-string-format-hhmmss-using-javascript/
+*/
+function convert_seconds_to_time(given_seconds) {
+
+    let dateObj = new Date(given_seconds * 1000);
+    let hours = dateObj.getUTCHours();
+    let minutes = dateObj.getUTCMinutes();
+    let seconds = dateObj.getSeconds();
+
+    let time_string = hours.toString().padStart(2, '0')
+        + ':' + minutes.toString().padStart(2, '0')
+        + ':' + seconds.toString().padStart(2, '0');
+
+    return time_string;
 }
