efeea61e329c8d6243bbdd5bb443cd78984c20e7
kshep425 <9432996+kshep425@users.noreply.github.com>
2019-11-24 16:47:06 -0500

    moved start exercise to it's own function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,7 @@
-function excercise() {
 
-}
-
-//wger_query("exercise");
-$("#start").click(function start_exercise(){
+$("#start").click(function(){
     $("#work_out_page").css({"display":"block"})
     $("#profile").css({"display":"none"})
     $("#start").css({"display":"none"})
-
-    let interval_time = 10;
-    display_time(30*60, "#total_workout_time")
-    excercise_ids = [91];
-    excercise_ids.forEach(id => {
-        wger_query("exercise/91");
-        console.log("Start Exercise")
-        display_time(interval_time, "#exercise_timer_section")
-        setTimeout(()=>{
-            console.log("Start Break")
-            its_break_time()
-        }, interval_time*1000)
-
-    });
-
-})
+    start_exercise()
+});
