59fda6650c51956a707e919fb81f4f118da2e4f4
kshep425 <9432996+kshep425@users.noreply.github.com>
2019-11-28 02:08:36 -0500

    Added the stop sound for first opening the page, moved functions to index.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
         $("#profile").hide();
         $("#start").hide();
         save_profile();
+        stop_sound("start_page_audio")
         progress();
         start_exercise();
     });
@@ -21,43 +22,7 @@ $(document).ready(function () {
         save_profile();
     })
 
-    function save_profile(){
-        profile = {
-            user_name: $("#user_name").val(),
-            goal: $("#goal_pref").val(),
-            intensity: $("#intensity_pref").val()
-        }
-        console.log(profile)
-        localStorage.setItem("profile", JSON.stringify(profile));
-    }
-
-    function load_profile() {
-        $("profile").show();
-        profile = JSON.parse(localStorage.getItem("profile"))
-        console.log(profile)
-        if (profile != undefined) {
-            $("#user_name").val(profile.user_name)
-            $("#goal_pref").val(profile.goal)
-            $("#intensity_pref").val(profile.intensity)
-        }
-    }
-
-    function progress() {
-        sec = 0;
-
-        var proBar = setInterval(function () {
-            let total_workout_time = ($($(":selected")[1]).attr("workout") === "demo")? 3 : 30;
-            let i = sec / (total_workout_time * 60)
-            $("#progress_bar").attr("value", i);
-            //console.log($("#progress_bar").attr("value"));
-            if (i >= 1) {
-                clearInterval(proBar);
-            }
-            sec++
-        }, 1000)
-
-    }
-
+    //play_sound("start_page_audio");
     load_profile();
     wger_query("exerciseimage", img_query_data);
 
