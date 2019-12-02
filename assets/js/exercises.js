758d446f745680f96f09bcd006a3467d833f8d5d
kshep425 <9432996+kshep425@users.noreply.github.com>
2019-12-02 09:07:41 -0500

    First run through of exercises;  Needs a lot of fixes if anyone is up to the challenge!  Add the embed links that would be really helpful.

---
+++ assets/js/exercises.js
@@ -0,0 +1,95 @@
+exercises = [{ //example
+        id: 1000,
+        name: "Jog in place",
+        description: "Switch lifting your legs up and down like you are jogging.",
+        gif_url: "https://thumbs.gfycat.com/DopeyShortBallpython-mobile.mp4",
+        category: "warmup",
+        level: "easy"
+    },
+    {
+        id: 1001,
+        name: "High knees",
+        description: "Raise right leg to hip while left foot is on the ground and alternate at medium tempo description",
+        gif_iframe: "<iframe src='https://giphy.com/embed/62aGqZoUJYtPsl0Hb0' width='480' height='270' frameBorder='0' class='giphy-embed' allowFullScreen></iframe> <p><a href='https://giphy.com/gifs/8fit-62aGqZoUJYtPsl0Hb0'>via GIPHY</a></p>",
+        category: "warmup",
+        level: "easy"
+    },
+    {
+        id: 1002,
+        name: "Lateral slide ",
+        description: "Get in a squat position and step laterally with the left foot and push with the right foot staying in the squat position and then alternate stepping with the right foot and pushing off with the left",
+
+        Gif_url: "https: //www.muscleandfitness.com/workouts/leg-exercises/videos/lateral-slide",
+        Gif_iframe: "< iframe src = 'http://content.jwplatform.com/players/TCCRPdtq-EPvWYbKj.html' width = '480' height = '270' frameborder = '0' scrolling = 'auto' > < /iframe>",
+        Category: "warmup",
+        level: "easy"
+    },
+    {
+        id: 1003,
+        name: "Body squats",
+        description: "Feet are shoulder width apart as you squat down to 90 degrees then explode up squeezing your glutes.",
+        gif_url: "https: //m.youtube.com/watch?v=m0GcZ24pK6k",
+        category: "warmup",
+        level: "easy"
+    },
+    {
+        id: 1004,
+        name: "Squat jumps",
+        description: "Squat down to 90 degrees and explode up with you arms reaching  for the ceailing description",
+        gif_url: "https: //m.youtube.com/watch?v=CVaEhXotL7M",
+        category: "warmup",
+        level: "easy"
+    },
+    {
+        id: 1005,
+        name: One leg jumps,
+        description: Standing on your left leg only,
+        do quick jumps and then
+        switch
+        to your right leg,
+
+        https: //m.youtube.com/watch?v=T_PnGqSbJAY
+            gif_url: "URL to gif"
+    },
+    {
+        id: 1006,
+        name: Butt kicks,
+        description: Jogging in place attempt to kick your but with each lift of the right and left leg ",
+
+        https: //m.youtube.com/watch?v=-dtvAxibgYQ
+            gif_url: "URL to gif"
+    },
+    {
+        id: 1007,
+        name: Feet chop
+        description: Standing in place knees slightly bent start one foot slightly off the ground and quickly alternate chopping the feetDescription ",
+
+        https: //basketballhq.com/athletic-position-foot-chops-drill
+            gif_url: "URL to gif"
+    },
+    {
+        id: 1008,
+        name: Power skips
+        description: Jump as high as you can driving your right knee into your chest and then alternate legs.escription ",
+        https: //m.youtube.com/watch?v=EMW4y-UiAvE
+            gif_url: "URL to gif"
+    },
+    {
+        id: 1009,
+        name: Pause squats
+        description: Squat to 90 degrees and hold.
+        https: //m.youtube.com/watch?v=OBOQQ71XrLk
+
+            gif_url: "URL to gif"
+    },
+    {
+        id: 1010,
+        name: Split jumps ",
+        description: Get in a lung position and jump up and alternate between your left and right leg.Description ",
+        https: //m.youtube.com/watch?v=fGbovzE6xVA
+
+            gif_url: "URL to gif"
+    }
+
+
+]
