a1c9f65078b43112c8519be286e4489437f99b5e
kshep425 <9432996+kshep425@users.noreply.github.com>
2019-12-03 02:40:37 -0500

    Fixed exercises file

--- assets/js/exercises.js
+++ assets/js/exercises.js
@@ -1,94 +1,91 @@
-exercises = [{ //example
+var demo_exercises = [{ //example
         id: 1000,
-        name: "Jog in place",
+        name: "Jog in Place",
         description: "Switch lifting your legs up and down like you are jogging.",
-        gif_url: "https://thumbs.gfycat.com/DopeyShortBallpython-mobile.mp4",
+        url: "https://thumbs.gfycat.com/DopeyShortBallpython-mobile.mp4",
         category: "warmup",
         level: "easy"
     },
     {
         id: 1001,
-        name: "High knees",
-        description: "Raise right leg to hip while left foot is on the ground and alternate at medium tempo description",
-        gif_iframe: "<iframe src='https://giphy.com/embed/62aGqZoUJYtPsl0Hb0' width='480' height='270' frameBorder='0' class='giphy-embed' allowFullScreen></iframe> <p><a href='https://giphy.com/gifs/8fit-62aGqZoUJYtPsl0Hb0'>via GIPHY</a></p>",
+        name: "High Knees",
+        description: "Raise right leg to hip while left foot is on the ground and alternate at medium tempo.",
+        url: "https://giphy.com/embed/62aGqZoUJYtPsl0Hb0",
         category: "warmup",
         level: "easy"
     },
     {
         id: 1002,
-        name: "Lateral slide ",
-        description: "Get in a squat position and step laterally with the left foot and push with the right foot staying in the squat position and then alternate stepping with the right foot and pushing off with the left",
-
-        Gif_url: "https: //www.muscleandfitness.com/workouts/leg-exercises/videos/lateral-slide",
-        Gif_iframe: "< iframe src = 'http://content.jwplatform.com/players/TCCRPdtq-EPvWYbKj.html' width = '480' height = '270' frameborder = '0' scrolling = 'auto' > < /iframe>",
+        name: "Lateral Slide",
+        description: "Get in a squat position and step laterally with the left foot and push with the right foot staying in the squat position and then alternate stepping with the right foot and pushing off with the left.",
+        url: "http://content.jwplatform.com/players/TCCRPdtq-EPvWYbKj.html",
         Category: "warmup",
         level: "easy"
     },
     {
         id: 1003,
-        name: "Body squats",
+        name: "Body Squats",
         description: "Feet are shoulder width apart as you squat down to 90 degrees then explode up squeezing your glutes.",
-        gif_url: "https: //m.youtube.com/watch?v=m0GcZ24pK6k",
+        url: "https://m.youtube.com/embed/m0GcZ24pK6k&output=embed",
+        embed: "<iframe width='560' height='315' src='https://www.youtube.com/embed/m0GcZ24pK6k' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>",
         category: "warmup",
         level: "easy"
     },
     {
         id: 1004,
-        name: "Squat jumps",
-        description: "Squat down to 90 degrees and explode up with you arms reaching  for the ceailing description",
-        gif_url: "https: //m.youtube.com/watch?v=CVaEhXotL7M",
+        name: "Squat Jumps",
+        description: "Squat down to 90 degrees and explode up with you arms reaching for the ceiling",
+        url: "https://m.youtube.com/embed/CVaEhXotL7M&output=embed",
         category: "warmup",
         level: "easy"
     },
     {
         id: 1005,
-        name: One leg jumps,
-        description: Standing on your left leg only,
-        do quick jumps and then
-        switch
-        to your right leg,
-
-        https: //m.youtube.com/watch?v=T_PnGqSbJAY
-            gif_url: "URL to gif"
+        name: "One Leg Jumps",
+        description: "Standing on your left leg only, do quick jumps and then switch to your right leg",
+        url: "https://m.youtube.com/embed/T_PnGqSbJAY",
+        category: "warmup",
+        level: "easy"
     },
     {
         id: 1006,
-        name: Butt kicks,
-        description: Jogging in place attempt to kick your but with each lift of the right and left leg ",
-
-        https: //m.youtube.com/watch?v=-dtvAxibgYQ
-            gif_url: "URL to gif"
+        name: "Butt Kicks",
+        description: "Jogging in place attempt to kick your but with each lift of the right and left leg",
+        url: "https://m.youtube.com/embed/-dtvAxibgYQ",
+        category: "warmup",
+        level: "easy"
     },
     {
         id: 1007,
-        name: Feet chop
-        description: Standing in place knees slightly bent start one foot slightly off the ground and quickly alternate chopping the feetDescription ",
-
-        https: //basketballhq.com/athletic-position-foot-chops-drill
-            gif_url: "URL to gif"
+        name: "Feet Chop",
+        description: "Standing in place knees slightly bent start one foot slightly off the ground and quickly alternate chopping the feet",
+        url: "https://basketballhq.com/athletic-position-foot-chops-drill",
+        category: "warmup",
+        level: "easy"
     },
     {
         id: 1008,
-        name: Power skips
-        description: Jump as high as you can driving your right knee into your chest and then alternate legs.escription ",
-        https: //m.youtube.com/watch?v=EMW4y-UiAvE
-            gif_url: "URL to gif"
+        name: "Power Skips",
+        description: "Jump as high as you can driving your right knee into your chest and then alternate legs",
+        url: "https://m.youtube.com/embed/EMW4y-UiAvE",
+        category: "warmup",
+        level: "easy"
     },
     {
         id: 1009,
-        name: Pause squats
-        description: Squat to 90 degrees and hold.
-        https: //m.youtube.com/watch?v=OBOQQ71XrLk
-
-            gif_url: "URL to gif"
+        name: "Pause Squats",
+        description: "Squat to 90 degrees and hold.",
+        url: "https://m.youtube.com/embed/OBOQQ71XrLk",
+        category: "warmup",
+        level: "easy"
     },
     {
         id: 1010,
-        name: Split jumps ",
-        description: Get in a lung position and jump up and alternate between your left and right leg.Description ",
-        https: //m.youtube.com/watch?v=fGbovzE6xVA
-
-            gif_url: "URL to gif"
+        name: "Split Jumps",
+        description: "Get in a lunge position and jump up and alternate between your left and right leg.",
+        url: "https://m.youtube.com/embed/fGbovzE6xVA",
+        category: "warmup",
+        level: "easy"
     }
 
 
